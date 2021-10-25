445d95a7bbbc5025e37d6b920923772aa1bb623b
Nathan Touroux <touroux.nathan@gmail.com>
2021-10-25 20:09:58 +0200

    enhance random example (constant memory)

--- examples/random/Pi.hs
+++ examples/random/Pi.hs
@@ -3,17 +3,18 @@
 -- | Compute the value of pi by generating random number.
 module Main where
 
+import Data.Bool (bool)
+import Data.Set (empty, insert, toList)
 import Dunai.Gloss
-import System.Random (RandomGen (split), mkStdGen)
-import System.Random.Stateful (uniformR)
+import System.Random (RandomGen (split), mkStdGen, uniformR)
 
 main :: IO ()
 main = do
   let gg = split $ mkStdGen 1
   playDunai (InWindow "Random Pi" (800, 600) (100, 100)) white 60 (network gg)
   where
-    circAt c = flip (uncurry translate) (Color c $ Circle 1)
-    r = 100
+    circAt (isinside, x, y) = translate (fromIntegral x) (fromIntegral y) $ Color (bool blue red isinside) $ Circle 0
+    r = 50 :: Double
     unitR = unfold $ uniformR (- r, r)
     net (g0, g1) = proc _ -> do
       i <- count -< ()
@@ -21,14 +22,13 @@ main = do
       y <- unitR g1 -< ()
       let isinside = x ** 2 + y ** 2 < r ** 2
       sumpi <- sumS -< if isinside then 1 else 0
-      poss <- mappendS -< if isinside then ([(x, y)], []) else ([], [(x, y)])
+      poss <- accumulateWith insert empty -< (isinside, round x, round y)
       let pi = 4 * sumpi / i :: Double
-      returnA -< (poss, pi)
+      returnA -< (poss, pi, i)
     network gg = proc _ -> do
-      (poss, pi) <- repeatMStream 100 $ net gg -< ()
-      returnA -< draw poss pi
-    draw (inside, outside) pi =
+      (poss, pi, i) <- repeatMStream 100 $ net gg -< ()
+      returnA -< draw (toList poss) pi i
+    draw poss pi i =
       Pictures $
-        (circAt red <$> inside)
-          <> (circAt blue <$> outside)
-          <> [Translate (-100) 150 . Scale 0.1 0.1 . Text $ "pi: 4*#red/(#red + #blue) = " <> show pi]
+        (circAt <$> poss)
+          <> [Translate (-100) 150 . Scale 0.1 0.1 . Text $ "pi: 4*#red/(#red + #blue) = " <> show pi, Translate (-100) 130 . Scale 0.1 0.1 . Text $ "sqrt(count): " <> show (sqrt i)]
